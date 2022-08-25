363ad6c7197270b854da367d15a53537a15d96f4
Phong Jacky Do <jacky.do@thangovn.com>
2022-08-25 10:59:06 +0700

    update blur

--- README.md
+++ README.md
@@ -11,8 +11,11 @@ sudo apt-get install ffmpeg libsm6 libxext6  -y
 
 # trained data language
 - https://github.com/tesseract-ocr/tessdata
-- [VietNam]: https://github.com/tesseract-ocr/tessdata/blob/main/vie.traineddata
+- VietNam: https://github.com/tesseract-ocr/tessdata/blob/main/vie.traineddata
+
 # copy into system
 sudo cp <vie.traineddata> /usr/share/tesseract-ocr/4.00/tessdata/
-
 # 
+
+# command test
+- python3 main.py -i testing.png -p thresh
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import argparse
 import cv2
 import os
  
-# -i file ảnh cần nhận dạng
-# -p tham số tiền xử lý ảnh (có thể bỏ qua nếu không cần). Nếu dùng: blur : Làm mờ ảnh để giảm noise, thresh: Phân tách đen trắng
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
         help="Đường dẫn đến ảnh muốn nhận dạng")
@@ -14,19 +12,21 @@ ap.add_argument("-p", "--preprocess", type=str, default="thresh",
         help="Bước tiền xử lý ảnh")
 args = vars(ap.parse_args())
 
+# read file
+
 # Read file and grayscale 
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  
 # Check pre-process
 # thresh hold white - black
-if args["preprocess"] == "thresh":
-        gray = cv2.threshold(gray, 0, 255,
-                cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# if args["preprocess"] == "thresh":
+gray = cv2.threshold(gray, 0, 255,
+        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
  
 # blur
-elif args["preprocess"] == "blur":
-        gray = cv2.medianBlur(gray, 3)
+# elif args["preprocess"] == "blur":
+gray = cv2.medianBlur(gray, 3)
  
 # temp store file to apply OCR
 filename = "{}.png".format(os.getpid())
